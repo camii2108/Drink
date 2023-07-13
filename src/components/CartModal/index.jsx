b7dca3a1e4dd417a78bae6e74393b7829737cdb6
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-07-13 19:44:38 -0300

    agregando los modales

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./CartModal.module.css";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons"; */
 
 function CartModal() {
+  const {isOpen, toogleModal } = useModal();
   return (
     <div className={styles.modalBg}>
       <div className={styles.modal}>
-      <FontAwesoneIcon icon={faXmarkCircle} className={}/>
-        <i className={styles.closeBtn}>x</i>
-        <h2>Mi Carrito</h2>
-        <section className={styles.modalBody}>
-          <div className={styles.modalDrinksList}>
+     
+      <h2>Mi Carrito</h2>
+      <section className={styles.modalBody}>
+      <i className={styles.icon}>x</i>
+          <div className={styles.modalDrinksListContainer}>
 
           {/* Contenido adicional del modal */}
             <article className={styles.card}>
-              <img src="ruta-de-la-imagen" alt="Nombre del producto" />
+              <img src="https://www.thecocktaildb.com/images/media/drink/tqxyxx1472719737.jpg" alt="Nombre del producto" />
               <span>Nombre del producto</span>
               <span>Precio</span>
               <div className={styles.counter}>
@@ -24,6 +24,7 @@ function CartModal() {
                 <span>Cantidad</span>
                 <button>+</button>
               </div>
+              {/* icono tachito de basura */}
              
             </article>
           </div>
@@ -31,9 +32,9 @@ function CartModal() {
             {/* Contenido adicional del modal */}
             <p> Subtotal: XXXXXXXX</p>
             <p> Total:XXXXXXX</p>
-            <div className="styles.btnContainer">
-            <button>Vaciar carrito</button>
-            <button>Confirmar Compra</button>
+            <div className={styles.btnContainer}>
+            <button className={styles.clear}>Vaciar carrito</button>
+            <button className={styles.confirmOrder}>Confirmar Compra</button>
             </div>
           </aside>
         </section>
---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,12 @@
+/* import {useState} from "react";
+
+
+export default function UseModal () {
+    const [isOpen, setIsOpen] = useState(ffalse);
+
+    function toogleModal () {
+        setIsOpen(!isOpen)
+    }
+    
+    return { isOpen, toogleModal }
+} */
