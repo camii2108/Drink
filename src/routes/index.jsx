df4e9b0c4c108e303c38f3771d3b8e62dad6974f
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-07-23 00:59:32 -0300

    tengo la base para el login, pero me pase a la rama de clase 10

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+{/* import SignIn from "../pages/User/SignIn";
+import SignUp from "../pages/User/SignUp"; */}
+
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
+{/*         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} /> */}
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
