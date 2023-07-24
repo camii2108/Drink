98fcc0937177b8d0bf830289ba78414a4306d5ac
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-07-24 00:18:29 -0300

    agregue context/AuthProvider, hooks/useAuth, modifique routes components/header

--- src/pages/User/SignIn/index.jsx
+++ src/pages/User/SignIn/index.jsx
@@ -2,9 +2,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -13,6 +10,8 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from '../Copyright';
 import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -20,6 +19,7 @@ import { Formik } from 'formik';
 const defaultTheme = createTheme();
 
 export default function SignIn() {
+  const { login } = useAuth();
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -42,69 +42,94 @@ export default function SignIn() {
 
         <Formik
         initialValues={{
-            name: "",
+            password: "",
             email:"",
         }}
-        validate={ () => {
+        validate={ (values) => {
             const errors = {};
-            if()
+            const regexpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  
+            if(!values.email) {
+                errors.email = "Email requerido"
+            }else if(!regexpEmail.test(values.email)) {
+                errors.email = "Email invalido";
+            }
+            if (!values.password) {
+                errors.password = "Contraseña requerida";
+            }
 
             return errors;
-        }
-
-        }
+        }}
+        //para esperar una respuesta con spiner setSubmiting
+        onSubmit={(values) =>{
+            login(values);
+        }}
 
 
         >
+        {
+            //funcion flecha disponibiliza un objeto
+            ({ 
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+            }) => (
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                
+                
+                <TextField
+                    margin="normal"
+                    /* {required} */
+                    fullWidth
+                    name="email"
+                    label="Email Address" 
+                    type="email"
+                    id="email"
+                    value={values.email}
+                    error= {errors.email &&  touched.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={errors.email && touched.email && errors.email }
+                  />
 
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="name"
-          label="nombre"
-          type="name"
-          id="name"
-          autoComplete="current-password"
-        />  
-          
-          <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-           
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+                  <TextField
+                margin="normal"
+                fullWidth
+                name="password"
+                label="password"
+                type="password"
+                id="password"
+                autoFocus
+                value={values.password}
+                error= {errors.password &&  touched.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={errors.password && touched.password && errors.password }//mensaje de error debajo, valida que los primeros sean true y e caso contrio que no devuleva nada, es para el caso de que no se cumpla la condicion para no usar un  if 
+              />  
+              
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Ingresar
+                  </Button>
+                  <Grid container>
+                    <Grid item>
+                      <Link to="/register" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+            )
+        }
+
+        
           </Formik>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
--- src/pages/User/SignUp/index.jsx
+++ src/pages/User/SignUp/index.jsx
@@ -2,9 +2,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -12,6 +9,9 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from '../Copyright';
+import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 
 
@@ -20,14 +20,8 @@ import Copyright from '../Copyright';
 const defaultTheme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const {register} = useAuth;
+
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -45,56 +39,100 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+           Registrate
           </Typography>
+
+          <Formik
+          initialValues={{
+              name: "",
+              email:"",
+              password : "",
+          }}
+          validate={ (values) => {
+              const errors = {};
+              const regexpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    
+              if(!values.email) {
+                  errors.email = "Email requerido"
+              }else if(!regexpEmail.test(values.email)) {
+                  errors.email = "Email invalido";
+              }
+              if (!values.name) {
+                  errors.name = "Nombre requerido";
+              }
+              if (!values.password) {
+                  errors.password = "Password requerido";
+              }
+  
+              return errors;
+          }}
+          //para esperar una respuesta con spiner 
+          onSubmit={(values) =>{
+              register(values);
+          }}
+  
+  
+          >
+          {
+              //funcion flecha disponibiliza un objeto
+              ({ 
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+              }) => (
+
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              
+              <Grid item xs={12}>
                 <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
+                margin="normal"
+                fullWidth
+                name="name"
+                label="nombre"
+                type="name"
+                id="name"
+                autoFocus
+                value={values.name}
+                error= {errors.email &&  touched.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={errors.name && touched.name && errors.name }
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                margin="normal"
+                /* {required} */
+                fullWidth
+                name="email"
+                label="Email Address" 
+                type="email"
+                id="email"
+                value={values.email}
+                error= {errors.email &&  touched.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={errors.email && touched.email && errors.email }
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
+                margin="normal"
+                /* {required} */
+                fullWidth
+                name="password"
+                label="password" 
+                type="password"
+                id="password"
+                value={values.password}
+                error= {errors.password &&  touched.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={errors.password && touched.password && errors.password }
                 />
               </Grid>
             </Grid>
@@ -104,16 +142,19 @@ export default function SignUp() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Registrate
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link to="/login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
           </Box>
+              )
+              }
+          </Formik>
         </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
